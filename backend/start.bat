4f48c6ab049b026d607179b803f2357c09f4fc57
yrsegal <yrsegal@gmail.com>
2015-04-27 11:33:39 -0400

    add start scripts and config

---
+++ backend/start.bat
@@ -0,0 +1,3 @@
+START /b /Python34/python.exe server.py
+START /b /Python34/python.exe main.py
+pause
---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ${0%/*}
+python3 server.py &
+python3 main.py &
+echo "Press enter to continue..."
+read
