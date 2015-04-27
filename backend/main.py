9f71537624a9b7e21de7e7e3db1f7422e24ace2e
yrsegal <yrsegal@gmail.com>
2015-04-27 11:18:50 -0400

    align text
    aesthetics \o/

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import os, mainux, mainwin
 def main():
-	if os.name is "nt": mainwin.main()
-	else:                mainux.main()
+  if os.name is "nt": mainwin.main()
+  else:                mainux.main()
 if __name__ == "__main__":    main()
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 import os, serverux, serverwin
 def main():
-	if os.name is "nt": serverwin.main()
-	else:                serverux.main()
+  if os.name is "nt": serverwin.main()
+  else:                serverux.main()
 if __name__ == "__main__":      main()
