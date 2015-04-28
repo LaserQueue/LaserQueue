0b4c7fd2ec03d433b5a0e6c166d02f5f8d4c5b9b
yrsegal <yrsegal@gmail.com>
2015-04-28 13:43:09 -0400

    automated ip discovery in default config

--- backend/initialize.py
+++ backend/initialize.py
@@ -1,8 +1,10 @@
 import json
 import os.path
+import socket
 
 def copyconf():
 	data = json.load(open(os.path.join("..", "www", "defaultconf.json")))
+	data["host"] = socket.gethostbyname(socket.getfqdn())+":8765"
 	json.dump(data, open(os.path.join("..", "www", "config.json"), "w"))
 
 def main():
