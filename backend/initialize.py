9b1db74ea2ee82688df57774a1c8877cf1c2b9d2
yrsegal <yrsegal@gmail.com>
2015-04-28 11:16:38 -0400

    config initialization added

--- backend/initialize.py
+++ backend/initialize.py
@@ -3,7 +3,7 @@ import os.path
 
 def copyconf():
 	data = json.load(open(os.path.join("..", "www", "defaultconf.json")))
-	json.dump(data, open(os.path.join("..", "www", "config.json")))
+	json.dump(data, open(os.path.join("..", "www", "config.json"), "w"))
 
 def main():
 	copyconf()
--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ${0%/*}
+python3 initialize.py
+sleep 1
 python3 server.py &
 python3 main.py 
 echo "Press enter to continue..."
