ea8ceff5c75b44288c17d0eb3dc477760ab953e9
sdaitzman <samuel@daitzman.com>
2015-05-01 11:32:38 -0400

    comments for _config.js

--- www/scripts/_config.js
+++ www/scripts/_config.js
@@ -5,10 +5,14 @@ var host, socket, materials, priorities, refreshRate, reconnectRate;
 
 // fetches config file from server
 var getConfigFile = $.getJSON('/config.json', function() {
+	// set host from host and port
 	host = "ws://" + getConfigFile.responseJSON["host"] + ":" + getConfigFile.responseJSON["port"];
 
+	// set materials and priorities in the same way
 	materials = getConfigFile.responseJSON["materials"];
 	priorities = getConfigFile.responseJSON["priorities"].reverse();
+
+	// log entire config file
 	logText("Config file follows:" + JSON.stringify(getConfigFile.responseJSON, null, 2));
 });
 
