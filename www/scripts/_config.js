002f8558902d6b89ad9e7e451b8faef7fc2b96cb
sdaitzman <samuel@daitzman.com>
2015-05-01 13:28:20 -0400

    unreverses priority list

--- www/scripts/_config.js
+++ www/scripts/_config.js
@@ -10,7 +10,7 @@ var getConfigFile = $.getJSON('/config.json', function() {
 
 	// set materials and priorities in the same way
 	materials = getConfigFile.responseJSON["materials"];
-	priorities = getConfigFile.responseJSON["priorities"].reverse();
+	priorities = getConfigFile.responseJSON["priorities"];
 
 	// set refreshRate and reconnectRate
 	refreshRate = getConfigFile.responseJSON["refreshRate"];
