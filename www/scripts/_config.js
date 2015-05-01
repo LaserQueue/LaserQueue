edbc5df70a4c705592dc3fb4d601dbf3761a95a7
sdaitzman <samuel@daitzman.com>
2015-05-01 11:32:27 -0400

    parse materials and priorities from config.json

--- www/scripts/_config.js
+++ www/scripts/_config.js
@@ -6,31 +6,13 @@ var host, socket, materials, priorities, refreshRate, reconnectRate;
 // fetches config file from server
 var getConfigFile = $.getJSON('/config.json', function() {
 	host = "ws://" + getConfigFile.responseJSON["host"] + ":" + getConfigFile.responseJSON["port"];
+
+	materials = getConfigFile.responseJSON["materials"];
+	priorities = getConfigFile.responseJSON["priorities"].reverse();
 	logText("Config file follows:" + JSON.stringify(getConfigFile.responseJSON, null, 2));
 });
 
 // pull table's first row out for insertion later
 var tableFirstRow = $(".cutting-table").html();
-
-// list of materials
-var materials = {
-	"w": "Thin Wood",
-	"tw": "Thick Wood",
-	"c": "Cardboard",
-	"a": "Acrylic",
-	"p": "Paper",
-	"f": "Fabric",
-	"o": "Other"
-};
-
-// list of priorities
-var priorities = [
-	"Lunch/After NuVu",
-	"Low", 
-	"Normal",
-	"Important",
-	"Urgent",
-	"Absolutely Required"
-].reverse();
 
 var refreshRate = 200;
