76e83e4023c12f577e95fc8ddccaf6859e940871
sdaitzman <samuel@daitzman.com>
2015-05-01 11:31:49 -0400

    log entire config file on start

--- www/scripts/_config.js
+++ www/scripts/_config.js
@@ -6,6 +6,7 @@ var host, socket, materials, priorities, refreshRate, reconnectRate;
 // fetches config file from server
 var getConfigFile = $.getJSON('/config.json', function() {
 	host = "ws://" + getConfigFile.responseJSON["host"] + ":" + getConfigFile.responseJSON["port"];
+	logText("Config file follows:" + JSON.stringify(getConfigFile.responseJSON, null, 2));
 });
 
 // pull table's first row out for insertion later
