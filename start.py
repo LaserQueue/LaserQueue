0abd9237eeba989ad2a21960ac40bebfb4db24ae
yrsegal <yrsegal@gmail.com>
2015-05-07 09:55:03 -0400

    nitpicking in --help

--- start.py
+++ start.py
@@ -3,11 +3,13 @@ import http.server
 from http.server import SimpleHTTPRequestHandler
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--local", help="Run from localhost", dest="local",
-	action="store_const",const=True,default=False)
+parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument("-p", "--port", help="Port to host from", dest="port",
 	default=80, type=int)
+parser.add_argument("-h", "--help", help="Show this help message and exit", dest="help",
+	action="store_const",const=True,default=False)
+parser.add_argument("-l", "--local", help="Run from localhost", dest="local",
+	action="store_const",const=True,default=False)
 parser.add_argument("-q", "--quiet", help="makes the script not give output", dest="shh",
 	action="store_const",const=True,default=False)
 parser.add_argument("-b", "--queue-backup", help="Backup queue and load from backup on start", dest="backup",
@@ -19,6 +21,10 @@ parser.add_argument("-s", "--skip-install", help="Skip package installation", de
 parser.add_argument("--install-all", help="Don't ask for confirmation on install", dest="all",
 	action="store_const",const=True,default=False)
 args = parser.parse_args()
+
+if args.help:
+	parser.print_help()
+	quit()
 
 selfpath = os.path.dirname(os.path.realpath(__file__))
 
