de71dcdd8804230f22758dc729f7cb877df566f8
sdaitzman <samuel@daitzman.com>
2015-05-07 16:38:36 -0400

    sets up for-now-insecure authentication
    DO NOT TRUST AUTHENTICATION YET. IT IS ABSOLUTELY NOT YET SECURE

--- www/scripts/_init.js
+++ www/scripts/_init.js
@@ -17,3 +17,24 @@ var jsonData;
 
 // initialize the modal by changing the title
 $('.notify-modal-title').html("Notification");
+
+// authentication modal
+$('.nuvu-logo').click(function() {
+	modalMessage('Authenticate', '
+		<form class="login-form">
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" placeholder="Password">
+			</div>
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</form>
+	');
+	$('.login-form').submit(function(event) {
+		event.preventDefault();
+		if($('#password').val() !== "imacoach") {
+			modalMessage("WHO DO YOU THINK YOU ARE", "I understand. You found paradise in America, you had a good trade, you made a good living. The police protected you and there were courts of law. You didn't need a friend like me. But, now you come to me, and you say: \"LaserQueue, give me justice.\" But you don't ask with respect. You don't offer friendship. You don't even think to call me Godfather. Instead, you come into my house on the day my daughter is to be married, you tell me you're a coach, and you LIE TO ME.");
+		} else {
+			coachMode();
+		}
+	});
+});
