6040394d4efd99353f32e299b7746df8ee0e4557
sdaitzman <samuel@daitzman.com>
2015-05-08 11:09:20 -0400

    adds a rickroll function

--- www/scripts/_functions.js
+++ www/scripts/_functions.js
@@ -67,6 +67,7 @@ function resetForm(form) {
 	}
 }
 
-function coachMode() {
-	
+function rickRoll() {
+	modalMessage('You goofed', '<iframe width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&disablekb=1&controls=0&loop=1&showinfo=0&iv_load_policy=3" frameborder="0" allowfullscreen></iframe>');
+	$('html').addClass('lol');
 }
