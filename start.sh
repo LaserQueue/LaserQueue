a22ab6143d9b7a42fe9f11708a63ee9cdd76fb8e
yrsegal <yrsegal@gmail.com>
2015-05-12 14:29:45 -0400

    make cleaning silent

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(/bin/pwd -P)
-python3 start.py $* && echo "import os;os.remove(\"/tmp/toscript.json\");os.remove(\"/tmp/topage.json\")" | python3
+python3 start.py $* && echo "import os;os.remove(\"/tmp/toscript.json\");os.remove(\"/tmp/topage.json\")" | python3 >/dev/null
 pkill python3
 pkill Python
