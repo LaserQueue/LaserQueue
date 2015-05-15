3acfea79701eea33d942e8130977362a7084200c
yrsegal <yrsegal@gmail.com>
2015-05-15 10:28:58 -0400

    why did I use python to rm
    nobody shall ever know
    including me dammit

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd $(/bin/pwd -P)
-python3 start.py $* && echo "import os;os.remove(\"/tmp/toscript.json\");os.remove(\"/tmp/topage.json\")" | python3 >/dev/null
+python3 start.py $*
+rm /tmp/toscript.json >/dev/null
+rm /tmp/topage.json >/dev/null
 pkill python3
 pkill Python
