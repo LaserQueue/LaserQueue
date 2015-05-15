8555b81b866dea7d314ce8635684d70c0ae71cdb
yrsegal <yrsegal@gmail.com>
2015-05-15 10:56:18 -0400

    remove auth blocker
    shift to sid

--- backend/serverux.py
+++ backend/serverux.py
@@ -46,24 +46,16 @@ def hello(websocket, path):
 					if message != None and "action" in messagedata:
 						if messagedata["action"] not in ["null", "auth", "uuddlrlrba"]:
 							print("saving message")
-						if messagedata["action"] == "auth":
-							if messagedata["args"][0].strip().rstrip() == open("password").read().strip().rstrip():
-								time.sleep(0.05)
-								yield from websocket.send(json.dumps({"action":"auth","success":True}))
-							else:
-								time.sleep(0.05)
-								yield from websocket.send(json.dumps({"action":"auth","success":False}))
-						else:
-							messagef = open(os.path.join(temppath, "toscript.json"), "w")
-							json.dump(messagedata, messagef)
-							messagef.close()
+						messagef = open(os.path.join(temppath, "toscript.json"), "w")
+						json.dump(messagedata, messagef)
+						messagef.close()
 
-							time.sleep(0.05)
+						time.sleep(0.05)
 
-							dataf = open(os.path.join(temppath, "topage.json"))
-							data = json.load(dataf)
-							dataf.close()
-							yield from websocket.send(json.dumps(data))
+						dataf = open(os.path.join(temppath, "topage.json"))
+						data = json.load(dataf)
+						dataf.close()
+						yield from websocket.send(json.dumps(data))
 		except:
 			pass
 			
