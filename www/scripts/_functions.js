498e8081a834404aff7a4d33493e3d798fc497ea
sdaitzman <samuel@daitzman.com>
2015-06-05 18:30:24 -0400

    _functions.js refactor

--- www/scripts/_functions.js
+++ www/scripts/_functions.js
@@ -1,4 +1,4 @@
-// put any utilities and functions here
+// put any utilities and functions etc. in here
 
 
 // logs text to devlog on page
@@ -24,7 +24,7 @@ function logText(text) {
 	}
 }
 
-// repopulate action button index
+// repopulate action button UUIDs
 function populateActions() {
 	logText('Populating actions');
 	$('.cutting-table-template tr td:nth-child(1)').each(function(index, el) {
@@ -79,15 +79,14 @@ function populateActions() {
 	});
 }
 
-// displays message in modal
+// displays message in a bootstrap modal
 function modalMessage(modalTitle, modalBody) {
 	$('.notify-modal-title').html(modalTitle);
 	$('.notify-modal-body').html(modalBody);
 	$('#notify-modal').modal();
 }
 
-// reset a form
-// with thanks to http://stackoverflow.com/questions/680241/resetting-a-multi-stage-form-with-jquery
+// reset a form with thanks to http://stackoverflow.com/questions/680241/resetting-a-multi-stage-form-with-jquery
 function resetForm(form) {
 	form.find('input:text, input:password, input[type=number], input:file, textarea').val(''); // removed 'select'
 	form.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
@@ -96,11 +95,24 @@ function resetForm(form) {
 	}
 }
 
+// adds SID to JSON and sends over websockets if the connection is stable
 function socketSend(jdata) {
-	jdata.sid = SID;
-	socket.send(JSON.stringify(jdata));
+	// if a session ID has been set and the socket is ready
+	if(typeof SID != "undefined" && socket.readyState == 1) {
+		jdata.sid = SID;
+		socket.send(JSON.stringify(jdata));
+	} else {
+		// if not sending, log why
+		if(typeof SID != "undefined") {
+			logText("socketSend() has been called, but SID is undefined.");
+		}
+		if(socket.readyState != 1) {
+			logText("socketSend() has been called, but socket.readyState is not 1. The socket is probably not connected yet.");
+		}
+	}
 }
 
+// read the function name, it says it all
 function rickRoll() {
 	if (easterEggs) {
 		modalMessage('Never gonna give you up', '<iframe width="420" height="315" src="http://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&disablekb=1&controls=0&loop=1&showinfo=0&iv_load_policy=3" frameborder="0" allowfullscreen></iframe>');
@@ -111,10 +123,12 @@ function rickRoll() {
 	}
 }
 
+// checks if the device is a touch device
 function isTouchDevice(){
 	return true == ('ontouchstart' in window || window.DocumentTouch && document instanceof DocumentTouch);
 }
 
+// ga() if Google Analytics is enabled
 function googleAnalytics(i, s, o, g, r, a, m){
 	if (config.google_analytics_key != '') {
 		ga(i, s, o, g, r, a, m);
