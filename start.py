035e9b70ce8ab8808931e5ebb5a3e77f110efffd
yrsegal <yrsegal@gmail.com>
2015-06-10 20:15:12 -0400

    more crossplatform stuff
    also, new update stuff

--- start.py
+++ start.py
@@ -16,8 +16,20 @@ def initFile(path, data=""):
 		newfile.write(data)
 		newfile.close()
 
-if __name__ == "__main__":
+def gPopen(cmd, stdin=None, stdout=None, stderr=None):
+	if os.name == "nt":
+		return subprocess.Popen(["py", "-3"] + cmd, stdin, stdout, stderr)
+	else:
+		return subprocess.Popen(["python3"] + cmd, stdin, stdout, stderr)
 
+def gSystem(cmd):
+	if os.name == "nt":
+		return os.system("py -3 "+cmd)
+	else:
+		return os.system("python3 "+cmd)
+
+
+if __name__ == "__main__":
 	temppath = tempfile.gettempdir()
 	initFile(os.path.join(temppath, "topage.json"))
 	initFile(os.path.join(temppath, "toscript.json"))
@@ -25,27 +37,31 @@ if __name__ == "__main__":
 	initFile(os.path.join(selfpath, "backend", "scache.json"), "{}")
 	initFile(os.path.join(selfpath, "www", "config.json"), "{}")
 
-	os.chdir(os.path.join(os.getcwd(), "backend"))
-	os.system("python3 initialize.py "+" ".join(sys.argv[1:]))
+	os.chdir("backend")
+	if gSystem("initialize.py "+" ".join(sys.argv[1:])) == 10:
+		os.chdir("..")
+		print("Update complete. Restarting program.")
+		gSystem("start.py ".join(sys.argv[1:]))
+		quit()
 
 	argvs = [i for i in sys.argv[1:] if i != "-q"]
 	FNULL = open(os.devnull, 'w')
 
 	output = FNULL if args.shh else None
 
-	backend_server = subprocess.Popen(["python3", "server.py"]+argvs, stdout=output, stderr=output)
-	backend_main = subprocess.Popen(["python3", "main.py"]+argvs, stdout=output, stderr=output)
+	backend_server = gPopen(["server.py"]+argvs, stdout=output, stderr=output)
+	backend_main = gPopen(["main.py"]+argvs, stdout=output, stderr=output)
 
 	time.sleep(0.5)
 
-	os.chdir(os.path.join(os.getcwd(), "..", "www"))
+	os.chdir(os.path.join("..", "www"))
 	if os.name != "nt" and os.geteuid() and args.port < 1024:
 		prompt = """Root required to host on ports up to 1023, enter your password to elevate permissions.
 (Use --port PORT to change ports.)
 Password: """
 		frontend = subprocess.Popen(["sudo", "-p", prompt, "python3", "-m", "http.server", str(args.port)], stdout=output, stderr=output)
 	else:
-		frontend = subprocess.Popen(["python3", "-m", "http.server", str(args.port)], stdout=output, stderr=output)
+		frontend = gPopen(["-m", "http.server", str(args.port)], stdout=output, stderr=output)
 	
 	while not backend_server.returncode and not backend_main.returncode and not frontend.returncode: time.sleep(0.001)
 	backend_server.kill()
