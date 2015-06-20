3e831d1224e8bca9d790d0701da1ee1058998b4b
yrsegal <yrsegal@gmail.com>
2015-06-20 19:45:07 -0400

    add WalkingConfig class
    not yet used
    
    looks upwards until it finds the file, allowing for concentric
    implementation
    not yet complete

--- backend/configloader.py
+++ backend/configloader.py
@@ -11,4 +11,20 @@ class Config:
 			lastmodtime = os.path.getctime(self.path)
 		return self.data[y]
 
+class WalkingConfig(Config):
+	def __init__(self, path):
+		filename = os.path.basename(path)
+		findpath = os.path.dirname(path)
+		self.path = None
+		while True:
+			if filename in os.listdir(findpath):
+				self.path = os.path.join(findpath, filename)
+				break
+			elif os.path.abspath(findpath) == os.path.sep:
+				raise FileNotFoundError(filename+" not found in directory tree.")
+			findpath = os.path.join(findpath, "..")
+		self.lastmodtime = os.path.getctime(self.path)
+		self.data = json.load(open(self.path))
+
+
 config = Config(os.path.join("..","www","config.json"))
