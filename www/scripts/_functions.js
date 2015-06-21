968a1a3bd50d66f4454aa392bbc8706a9e221023
sdaitzman <samuel@daitzman.com>
2015-06-21 19:18:01 -0400

    formatting fixes to reduce jscs' anger

--- www/scripts/_functions.js
+++ www/scripts/_functions.js
@@ -1,53 +1,54 @@
 // put any utilities and functions etc in here
 
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined' ? args[number] : match;
-    });
-  };
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined' ? args[number] : match;
+		});
+	};
 }
+
 
 if (!String.prototype.repeat) {
   String.prototype.repeat = function(count) {
-    'use strict';
-    if (this === null) {
-      throw new TypeError('can\'t convert ' + this + ' to object');
-    }
-    var str = '' + this;
-    count = +count;
-    if (count != count) {
-      count = 0;
-    }
-    if (count < 0) {
-      return str;
-    }
-    if (count == Infinity) {
-      throw new RangeError('repeat count must be less than infinity');
-    }
-    count = Math.floor(count);
-    if (str.length === 0 || count === 0) {
-      return '';
-    }
-    // Ensuring count is a 31-bit integer allows us to heavily optimize the
-    // main part. But anyway, most current (August 2014) browsers can't handle
-    // strings 1 << 28 chars or longer, so:
-    if (str.length * count >= 1 << 28) {
-      throw new RangeError('repeat count must not overflow maximum string size');
-    }
-    var rpt = '';
-    for (;;) {
-      if ((count & 1) == 1) {
-        rpt += str;
-      }
-      count >>>= 1;
-      if (count === 0) {
-        break;
-      }
-      str += str;
-    }
-    return rpt;
+	'use strict';
+	if (this === null) {
+	  throw new TypeError('can\'t convert ' + this + ' to object');
+	}
+	var str = '' + this;
+	count = +count;
+	if (count != count) {
+	  count = 0;
+	}
+	if (count < 0) {
+	  return str;
+	}
+	if (count == Infinity) {
+	  throw new RangeError('repeat count must be less than infinity');
+	}
+	count = Math.floor(count);
+	if (str.length === 0 || count === 0) {
+	  return '';
+	}
+	// Ensuring count is a 31-bit integer allows us to heavily optimize the
+	// main part. But anyway, most current (August 2014) browsers can't handle
+	// strings 1 << 28 chars or longer, so:
+	if (str.length * count >= 1 << 28) {
+	  throw new RangeError('repeat count must not overflow maximum string size');
+	}
+	var rpt = '';
+	for (;;) {
+	  if ((count & 1) == 1) {
+		rpt += str;
+	  }
+	  count >>>= 1;
+	  if (count === 0) {
+		break;
+	  }
+	  str += str;
+	}
+	return rpt;
   };
 }
 
