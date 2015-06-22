9a3c19be96ab43d61e0d2075c18f12140fba0f76
sdaitzman <samuel@daitzman.com>
2015-06-22 09:59:54 -0400

    makes autoprefixer code prettier

--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,7 @@ gulp.task('sass', function() {
 	return gulp.src('./www/lib/scss/styles.scss')
 		.pipe(sourcemaps.init())
 			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-			.pipe(
-				autoprefixer(
-					'last 2 version',
-					'safari 5',
-					'ie 8',
-					'ie 9',
-					'opera 12.1',
-					'ios 6',
-					'android 4'
-				)
-			)
+			.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 			.pipe(rename({suffix: '.min'}))
 		.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./www/dist/css'))
