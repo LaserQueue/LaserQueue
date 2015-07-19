77d074af43ba5369dba0ee9fd87616503c4b7b74
sdaitzman <samuel@daitzman.com>
2015-07-18 22:51:04 -0400

    20% reduction in output-to-sass ratio
    files are tinier

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var csso         = require('gulp-csso');
 var notify       = require('gulp-notify');
 var jscs         = require('gulp-jscs');
 var jscsStylish  = require('gulp-jscs-stylish');
@@ -21,8 +22,9 @@ var sizereport   = require('gulp-sizereport');
 gulp.task('sass', function() {
 	return gulp.src('./www/lib/scss/styles.scss')
 		.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+			.pipe(sass().on('error', sass.logError))
 			.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+			.pipe(csso())
 			.pipe(rename({suffix: '.min'}))
 		.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./www/dist/css'))
