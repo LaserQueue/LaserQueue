bb7e4079508fedf14bb00d2037e5d05b2e853450
sdaitzman <samuel@daitzman.com>
2015-07-22 08:43:27 -0400

    fixes comment in libraries.js

--- www/lib/scripts/libraries.js
+++ www/lib/scripts/libraries.js
@@ -1,6 +1,4 @@
-// main program
-
-// libraries first
+// libraries
 @@include('../../bower_components/jquery/dist/jquery.min.js')
 @@include('../../bower_components/bootstrap/dist/js/bootstrap.min.js')
 @@include('../../bower_components/transparency/dist/transparency.min.js')
