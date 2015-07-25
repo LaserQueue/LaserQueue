0041310e946e236149ca82de2d0eb26e62f6054c
yrsegal <yrsegal@gmail.com>
2015-07-24 22:00:33 -0400

    add plugin getting
    this… shit, this was actually the hardest part o.o

---
+++ scripts/plugins.py
@@ -0,0 +1,22 @@
+import sys, os
+
+from config import *
+cprintconf.color = bcolors.DARKGRAY
+cprintconf.name = "Plugins"
+
+PLUGINDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "plugins"))
+sys.path.append(PLUGINDIR)
+
+def getPlugins():
+	pluginFiles = os.listdir(PLUGINDIR)
+	pluginPyfiles = filter(lambda x: x.endswith(".py"), pluginFiles)
+
+	def tryImport(name):
+		try:
+			return __import__(name)
+		except Exception as e:
+			cprint(tbformat(e, "Error importing {}:".format(name)), color=bcolors.DARKRED)
+
+	pluginModules = (tryImport(x[:-3]) for x in pluginPyfiles)
+	pluginModules = filter(lambda module: (hasattr(module, "upkeep") or hasattr(module, "socketCommands")), pluginModules)
+	return pluginModules
