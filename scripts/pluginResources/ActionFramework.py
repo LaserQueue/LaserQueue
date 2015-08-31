b0b74b4c920dcd7d4c514228ecc1e249551ca591
sdaitzman <samuel@daitzman.com>
2015-08-31 07:30:25 -0400

    deletes everything not design

--- scripts/pluginResources/ActionFramework.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(
-	os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
-from jsonhandler import SocketCommand, any_type, any_number
--- scripts/pluginResources/ParseArgs.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(
-	os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
-from parseargv import *
