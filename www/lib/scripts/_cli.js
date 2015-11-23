3af0fde8c924901fd4121af30f3baec00c57dbe6
yrsegal <yrsegal@gmail.com>
2015-11-22 22:57:28 -0500

    finalize cli things

--- www/lib/scripts/_cli.js
+++ www/lib/scripts/_cli.js
@@ -2,26 +2,56 @@ function Command(match, execute) {
 	if (!isCommandDefinition(match)) throw "Invalid command. ({match})".format(match);
 	this.match = match;
 	var definition = parseCommandDefinition(match);
+	if (typeof definition === 'string') definition = {};
+	this.command = definition;
 	this.name = definition.name;
-	this.level = definition.level;
-	this.args = definition.args;
 	this.docstr = "Usage: "+match;
 	this.execute = execute;
 }
 
-/**
- * Argument types:
- * <name> -> required arg (you may not have any of these after a required arg)
- * [name] -> optional arg
- * [*literal] -> optional literal (passed as a true/false)
- * ... -> capture all remaining data as literal
- * literal -> catch as literal
- * 
- * a command must follow either of the following formats
- * commands prepended with # are only runnable as admin
- * commands not prepended with # must be prepended with $
- * names must only be comprised of characters that match /\w/
- */
+Command.prototype.toString = function toString() {
+	return this.match;
+};
+
+Command.prototype.setDocstr = function setDocstr(string) {
+	this.docstr = string.replace(/%s/, this.match);
+};
+
+Command.prototype.parse = function parse(inp) {
+	var inpParsed = extractCommand(inp);
+	if (typeof inpParsed === 'string') inpParsed = [];
+	var args = matchArguments(this.command, inpParsed);
+	if (typeof args === 'string') args = {};
+	return args;
+};
+
+Command.prototype.matches = function matches(inp) {
+	var inpParsed = extractCommand(inp);
+	if (typeof inpParsed === 'string') inpParsed = [];
+	var args = matchArguments(this.command, inpParsed);
+	return typeof args !== 'string';
+};
+
+Command.prototype.run = function run(inp) {
+	if (this.matches(inp)) {
+		return this.execute(this.parse(inp));
+	}
+	return false;
+};
+
+
+// argument types:
+// <name> -> required arg (you may not have any of these after a required arg)
+// [name] -> optional arg
+// [*literal] -> optional literal (passed as a true/false)
+// ... -> capture all remaining data as literal
+// literal -> catch as literal
+
+// a command must follow either of the following formats
+// commands prepended with # are only runnable as admin
+// commands not prepended with # must be prepended with $
+// names must only be comprised of characters that match /\w/
+ 
 function isCommandDefinition(string) {
 	string = string.trim();
 	var name = string.search(/^[\$#]\s*\w+\b/);
@@ -31,7 +61,7 @@ function isCommandDefinition(string) {
 	var keys = string.trim().split(/\s/);
 	var optflag = false;
 	var captureflag = false;
-	for (index in keys) {
+	for (var index in keys) {
 		var key = keys[index];
 		if (key.search(/^\[\*?\w+\]$/) !== -1) {
 			optflag = true;
@@ -56,12 +86,12 @@ function parseCommandDefinition(string) {
 	string = string.trim();
 	var name = string.match(/^[\$#]\s*\w+\b/)[0];
 	ret.name = name.replace(/[\$#]\s*/, "");
-	ret.level = name[0] == "#" ? 1 : 0;
+	ret.level = name[0] === "#" ? 1 : 0;
 
 	string = string.replace(/^[\$#]\s*\w+\b/, "");
 	var keys = string.trim().split(/\s+/);
 
-	for (index in keys) {
+	for (var index in keys) {
 		var key = keys[index];
 		if (key.search(/^\[\w+\]$/) !== -1) {
 			ret.args.push({
@@ -98,7 +128,7 @@ function extractCommand(string) {
 	var currentString = "";
 	var stringArgs = [];
 	var keys = string.trim().split(/\s+/);
-	for (index in keys) {
+	for (var index in keys) {
 		var key = keys[index];
 		if (!inString && key.search(/^(\\\\)*\"[^((\\\\)*\")]/) !== -1) {
 			inString = true;
@@ -121,26 +151,26 @@ function extractCommand(string) {
 
 function matchArguments(command, args) {
 	var ret = {};
-	for (argindex in command.args) {
+	for (var argindex in command.args) {
 		var arg = command.args[argindex];
 		var inparg = args[argindex];
 		if (!argindex) {
-			if (command.name != inparg) {
+			if (command.name !== inparg) {
 				return "error.incorrectName";
 			}
 		} else if (arg.type === 'lit') {
-			if (inparg != arg.name) {
+			if (inparg !== arg.name) {
 				return "error.missingLiteral";
 			}
 		} else if (arg.type === 'req' || arg.type === 'opt') {
 			ret[arg.name] = inparg;
 		} else if (arg.type === 'optlit') {
-			if (inparg != arg.name && inparg != null) {
+			if (inparg !== arg.name && inparg !== null) {
 				ret[arg.name] = null;
 			} else {
 				return "error.badLiteral";
 			}
-		} else if (arg.type == 'splat') {
+		} else if (arg.type === 'splat') {
 			ret[arg.name] = args.slice(argindex+1).join(" ");
 		} else {
 			return "error.badAction";
@@ -148,15 +178,3 @@ function matchArguments(command, args) {
 	}
 	return ret;
 }
-
-Command.prototype.toString = function toString() {
-	return this.match;
-}
-
-Command.prototype.setDocstr = function setDocstr(string) {
-	this.docstr = string.replace(/%s/, this.match)
-};
-
-Command.prototype.parse = function parse(inp) {
-
-};
